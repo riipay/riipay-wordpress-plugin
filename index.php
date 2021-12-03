26450799669cc9fe95a503e4273ba868eb649d4d
Hui Voon Ang <hvang@ryu.my>
2021-12-03 11:08:31 +0800

    Visibility Control

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Riipay for Woocommerce
  * Description: Start accepting payments with zero-interest instalments using credit or debit cards now!
- * Version: 1.0.13
+ * Version: 1.0.14
  * Author: Riipay
  * Author URI: https://riipay.my
  * WC requires at least: 3.2
